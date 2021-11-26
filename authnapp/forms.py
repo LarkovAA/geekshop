9315d9620989130030f2e52bcef4e26e00f59c19
LarkovAA <lexlar@mail.ru>
2021-11-26 19:25:12 +0400

    Добавил файлы и валидатор.

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.exceptions import ValidationError
+
 from .models import ShopUser
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -13,6 +15,11 @@ class ShopUserLoginForm(AuthenticationForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
 
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        if not data.isalpha():
+            raise ValidationError('Имя пользователя не может содержать цифры')
+        return data
 
 class ShopUserLogoutForm(UserCreationForm):
     class Meta:
