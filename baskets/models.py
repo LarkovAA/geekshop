416d6d99e72639fb8c1531f2d1cd9b50b825cc66
LarkovAA <lexlar@mail.ru>
2022-01-16 19:08:07 +0400

    Кеширование при помощи Memcached

--- baskets/models.py
+++ baskets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.functional import cached_property
+
 from authnapp.models import ShopUser
 from product.models import Product
 
@@ -15,7 +17,7 @@ from product.models import Product
 class Basket(models.Model):
     # objects = BasketQuerySet.as_manager()
 
-    user = models.ForeignKey(ShopUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(ShopUser, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     create_timestamp = models.DateTimeField(auto_now_add=True)
@@ -24,34 +26,23 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     def summ(self):
         return self.quantity * self.product.price
 
     def total_summ(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.summ() for basket in baskets)
 
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.quantity for basket in baskets)
 
-    # def delete(self, using=None, keep_parents=False, *args, **kwargs):
-    #     super(Basket, self).delete(*args, **kwargs)
-    #     self.product.quantity += self.quantity
-    #     self.save()
-    #
-    #
-    # def save(self, force_insert=False, force_update=False, using=None,
-    #          update_fields=None, *args, **kwargs):
-    #     if self.pk:
-    #         get_item = instance.get_item(int(instance.pk))
-    #         instance.product.quantity -= instance.quantity - get_item
-    #
-    #     else:
-    #         self.product.quantity -= self.quantity
-    #     self.product.save()
-    #     super(Basket, self).save(*args, **kwargs)
-    #
     @staticmethod
     def get_item(pk):
         return Basket.objects.get(pk=pk).quantity
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -207,3 +207,16 @@ if DEBUG:
        'template_profiler_panel.panels.template.TemplateProfilerPanel',
    ]
 
+if os.name == 'posix':
+   CACHE_MIDDLEWARE_ALIAS = 'default'
+   CACHE_MIDDLEWARE_SECONDS = 120
+   CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+
+   CACHES = {
+       'default': {
+           'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+           'LOCATION': '89.108.103.251:11211',
+       }
+   }
+
+LOW_CACHE = True
